fafa139a4be551ba767a9be04b92117606c9db1d
Maxpa1n <775126459@qq.com>
2020-10-16 20:10:43 +0800

    1016

--- code/seq2seq.sh
+++ code/seq2seq.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=2,3,4,5  \
 python -u run_seq2seq.py --data_dir ../data   \
-        --src_file test_1010.json   \
+        --src_file train_1010.json   \
 	--dev_file test_1010.json  \
         --model_type unilm  \
         --model_name_or_path ../user_data/pretrain/   \
@@ -11,10 +11,14 @@ python -u run_seq2seq.py --data_dir ../data   \
         --do_train   \
         --do_lower_case   \
         --train_batch_size 38   \
-	--dev_batch_size 20 \
-        --model_recover_path ../out_model/model.18.bin \
-        --num_train_epochs 30  \
-        --mask_prob 0.2 \
+	--dev_batch_size 30 \
+        --model_recover_path ../LR_model/model.30.bin \
+        --num_train_epochs 20  \
+	--warmup_proportion 0.1 \
+        --skipgram_size 6  \
+        --skipgram_prb 0.3 \
+	--mask_prob 0.5 \
         --max_pred 40 \
         --learning_rate 5e-5 \
-	--label_smoothing 0.1
+	--label_smoothing 0.15 \
+
